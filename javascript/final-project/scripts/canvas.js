4789852d330966bd2dd6ba41bb7ec939f8426011
Aayush1011 <aayush1000adhikari@gmail.com>
2023-02-13 09:18:39 +0545

    fixed js final project screen size

--- javascript/final-project/scripts/canvas.js
+++ javascript/final-project/scripts/canvas.js
@@ -4,8 +4,8 @@ class Canvas {
     this.ctx = this.canvas.getContext("2d");
     this.ctx.imageSmoothingEnabled = true;
     this.ctx.imageSmoothingQuality = "high";
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
+    this.canvas.width = 1488;
+    this.canvas.height = 746;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
   }
