c7936afc51bfb842be4925955d8129fb6091c656
Aayush1011 <aayush1000adhikari@gmail.com>
2023-02-21 08:27:20 +0545

    fixed pointer follower

--- react/session-4-cursor-follower/src/App.jsx
+++ react/session-4-cursor-follower/src/App.jsx
@@ -1,23 +1,25 @@
-import "./session-4__cursor-follower/reset.css";
+import "./session-4/reset.css";
 
-import PointerFollower from "./session-4__cursor-follower/pointerFollower";
+import PointerFollower from "./session-4/PointerFollower";
 
-import cloudIcon from "./session-4__cursor-follower/assets/cloud-solid.svg";
-import gamepadIcon from "./session-4__cursor-follower/assets/gamepad-solid.svg";
-import playIcon from "./session-4__cursor-follower/assets/play-solid.svg";
-import rocketIcon from "./session-4__cursor-follower/assets/rocket-solid.svg";
+import cloudIcon from "./session-4/assets/cloud-solid.svg";
+import gamepadIcon from "./session-4/assets/gamepad-solid.svg";
+import playIcon from "./session-4/assets/play-solid.svg";
+import rocketIcon from "./session-4/assets/rocket-solid.svg";
 
 function App() {
-  <div style={{ display: "flex", flexDirection: "column", rowGap: "20px" }}>
-    <div style={{ display: "flex", columnGap: "20px" }}>
-      <PointerFollower icon={rocketIcon} background="green" />
-      <PointerFollower icon={playIcon} background="yellow" />
+  return (
+    <div style={{ display: "flex", flexDirection: "column", rowGap: "20px" }}>
+      <div style={{ display: "flex", columnGap: "20px" }}>
+        <PointerFollower icon={rocketIcon} background="green" />
+        <PointerFollower icon={playIcon} background="yellow" />
+      </div>
+      <div style={{ display: "flex", columnGap: "20px" }}>
+        <PointerFollower icon={gamepadIcon} background="red" />
+        <PointerFollower icon={cloudIcon} background="blue" />
+      </div>
     </div>
-    <div style={{ display: "flex", columnGap: "20px" }}>
-      <PointerFollower icon={gamepadIcon} background="red" />
-      <PointerFollower icon={cloudIcon} background="blue" />
-    </div>
-  </div>;
+  );
 }
 
 export default App;
